91e89c0d7f576ecdc01d4da0d50a7c9ee520af06
Nazar Pidhirnyi <nazar.pidhirnyi@gmail.com>
2020-11-10 19:01:05 +0100

    added formik

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Paginatior from '../common/Paginator/Paginator'
 import User from './User'
 import { userType } from '../../types/types'
+import { Field, Form, Formik } from 'formik'
 
 
 type propsType = {
@@ -18,6 +19,7 @@ type propsType = {
 const Users: React.FC<propsType> = (props) => {
 
   return <div>
+    <UsersSearchForm />
     <Paginatior totalUsersCount={props.totalUsersCount} pageSize={props.pageSize} onPageChanged={props.onPageChanged} currentPage={props.currentPage} />
 
     {
@@ -26,11 +28,42 @@ const Users: React.FC<propsType> = (props) => {
   </div>
 }
 
+type UserSearchFormObjectType = {
+  term: string
+}
 
+const userSearchFormValidate = (values) => {
+  const errors = {};
+  return errors;
+}
 
+const UsersSearchForm = () => {
 
+  const submit = (values: UserSearchFormObjectType, { setSubmitting }: {setSubmitting: (isSubmitting: boolean) => void}) => {
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 400)
+  }
 
-
+  return (
+    <div>
+      <Formik
+        initialValues={{ term: ''}}
+        validate={userSearchFormValidate}
+        onSubmit={submit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="term" />
+            <button type="submit" disabled={isSubmitting}>
+              Find
+           </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
+}
 
 
 export default Users
